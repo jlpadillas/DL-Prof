427db44cf80448d367455d831e904305e9ff85c0
Juan Luis Padilla Salome <jps55@alumnos.unican.es>
2021-03-31 12:19:27 +0200

    Cambios en el bash de perf. Mejoras tontas.

--- 1.mat_mul/src/perf.sh
+++ 1.mat_mul/src/perf.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 
+# ------------------------------------------------------------------------ #
 
+
+
+
+# ------------------------------------------------------------------------ #
+# Error handling
+set -e
+
+err_report() {
+    echo "Error on line $1"
+}
+
+trap 'err_report $LINENO' ERR
+
+echoerr() { printf "%s\n" "$*" >&2; }
+# echoerr hello world
+
+# trap 'echoerr $LINENO' ERR
+
+
+# ------------------------------------------------------------------------ #
 # Usage
 usage () {
     cat <<HELP_USAGE
-Usage: perf.sh [[Program] | [Options]]
+Usage: perf.sh [[Program] [Parameters] | [Options]]
 
 Program:
-    Utiliza el comando perf para medir las operaciones de punto flotante que
-    se producen durante la ejecución del programa [Program].
+    It uses the perf command to measure floating point operations that occur
+    during the execution of the program [Program].
 
-    Ejecuta el programa "mat_mul.py" pasándole el mismo parámetro que se introduzca
-    al ejecutar este programa.
+    The program accepts binary and python files.
 
 Options:
    -h, --help
@@ -24,16 +44,45 @@ if [[ -z $1 ]] || [[ $1 = "--help" ]] || [[ $1 = "-h" ]]; then
     exit
 fi
 # ------------------------------------------------------------------------ #
-
 # Variables
-PYTHON=`which python3`
+
 PERF=`which perf`
-EVENTS=`perf list | grep fp_ | mawk '{print}' ORS=',' | sed 's/ //g'`cycles\
-,instructions
-SRC_DIR="src"
-# PROGRAM="mat_mul.py"
+if [[ -z $PERF ]]; then
+    echo "[ERROR] The program needs to have perf installed." >&2
+    exit -1
+else
+    EVENTS=`perf list | grep fp_ | mawk '{print}' ORS=',' | sed 's/ //g'`
+    EVENTS=instructions,cycles,${EVENTS%,}
+fi
+
+# Saves the name of the program to execute and its parameters
 PROGRAM=$1
-PARAMS=$2
+PARAMS=""
+if [ "$#" -gt 1 ]; then
+    PARAMS="${@:2}"
+fi
+
+# Check if the program has an extension (.py, .c, .something)
+EXEC=""
+FILE_NAME=$PROGRAM
+FILE_EXTN=""
+if [[ $PROGRAM == *"."* ]]; then # It has an extension
+    FILE_NAME=`echo "$PROGRAM" | cut -d'.' -f1`
+    FILE_EXTN=`echo "$PROGRAM" | cut -d'.' -f2`
+
+    # Check if it is a python program and modify the exec
+    if [[ $FILE_EXTN == "py" ]]; then
+        EXEC=`which python3`
+    else
+        echoerr "[ERROR] Extension '$FILE_EXTN' not supported."
+        exit -1
+    fi
+fi
+
+SRC_DIR="src"
+
+exit
+
 # ------------------------------------------------------------------------ #
 
 # Start singlethread measure
